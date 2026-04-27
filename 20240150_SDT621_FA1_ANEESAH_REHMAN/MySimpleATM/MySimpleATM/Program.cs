cfa4074fc1024806e69ec9fd23286a70581c7fda
charming-leather <aneesahr64@gmail.com>
2026-04-27 21:08:43 +0200

    Add files via upload

---
+++ 20240150_SDT621_FA1_ANEESAH_REHMAN/MySimpleATM/MySimpleATM/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("CTU SIMPLE ATM SYSTEM\n");
+
+        // Prompting user name
+        Console.Write("HI, WHAT IS YOUR NAME? ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"\nWELCOME {name.ToUpper()}!");
+
+        // Promting account balance
+        Console.Write("Enter account balance: ");
+        double balance = Convert.ToDouble(Console.ReadLine());
+
+        // Promting how much want to draw
+        Console.Write("Enter withdrawal amount: ");
+        double withdrawal = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine();
+
+        // Checking if account has money or insufficient funds
+        if (withdrawal <= balance)
+        {
+            balance = balance - withdrawal;
+
+            Console.WriteLine("Withdrawal successful!");
+
+            // Display updated balance after money drawed
+            Console.WriteLine("Updated Balance: " + balance.ToString("N2"));
+
+            // Display transaction time and date 
+            DateTime transactionTime = DateTime.Now;
+            Console.WriteLine("Transaction Time: " + transactionTime.ToString("dd MMM yyyy HH:mm:ss"));
+        } // close if
+        else
+        {
+            Console.WriteLine("Insufficient funds! Withdrawal declined.");
+        } // close else
+
+        Console.WriteLine("\nThank you for using CTU ATM System.");
+    } // close Main
+}// close program
